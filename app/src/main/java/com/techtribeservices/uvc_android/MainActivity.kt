b5dc34b7c1fa3c5c012b87c6012225873e46d12b
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-01-24 11:42:59 +0530

    Added a simple UI. Also added USB Manager and added the code to show connected device data in Logs

--- app/src/main/java/com/techtribeservices/uvc_android/MainActivity.kt
+++ app/src/main/java/com/techtribeservices/uvc_android/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.techtribeservices.uvc_android
 
+import android.content.Context
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.techtribeservices.uvc_android.ui.theme.UVC_AndroidTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+data class DeviceClass(var deviceName:String = "",
+    var deviceId:Any = "",
+    var deviceClass: Any = "")
+
+var TAG = "CAM"
+private const val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
 
 class MainActivity : ComponentActivity() {
+    val manager = getSystemService(Context.USB_SERVICE) as UsbManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             UVC_AndroidTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +45,57 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CameraComponent()
                 }
+            }
+
+            val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+            if (device != null) {
+                DeviceClass(device.deviceName, device.deviceId, device.deviceClass)
+                Log.d(TAG, device.deviceName)
+                Log.d(TAG, "device Id - ${device.deviceId}")
+                Log.d(TAG, "device class - ${device.deviceClass}")
+                Log.d(TAG, "device subclass - ${device.deviceSubclass}")
+                Log.d(TAG, "device protocol - ${device.deviceProtocol}")
+            } else {
+                Toast.makeText(this, "device not connected",Toast.LENGTH_LONG).show()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CameraComponent(modifier: Modifier = Modifier) {
+    var isDetected by remember {
+        mutableStateOf(false)
+    }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(if(isDetected) "Camera detected" else "Camera POC",
+            modifier = modifier)
+
+        Button(onClick = {
+            cameraFunction()
+            print("button on click here")
+            isDetected = !isDetected
+        }) {
+            Text(text = "Click Me")
+        }
+    }
+}
+
+// camera functionality
+private fun cameraFunction() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     UVC_AndroidTheme {
-        Greeting("Android")
+        CameraComponent()
     }
 }
